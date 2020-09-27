01aebd1c40cf02a0c1592680e612276acce95eb0
Kevin Tan <tankevin@stanford.edu>
2020-09-27 14:28:07 -0400

    🎨 Call functions from the module

--- Payload/serialCommandNode.py
+++ Payload/serialCommandNode.py
@@ -1,23 +1,24 @@
 import json
-from utils import get_redis_client, config, run
+
+import utils
 
 
 async def main():
-    sub = await get_redis_client()
-    pub = await get_redis_client()
+    sub = await utils.get_redis_client()
+    pub = await utils.get_redis_client()
     (pattern,) = await sub.subscribe(
-        config["CHANNELS"]["FC-IN"]
+        utils.config["CHANNELS"]["FC-IN"]
     )  # read all channels prefixed with `SOME_`
 
     while await pattern.wait_message():
         data = await pattern.get()
         message = json.loads(data)
-        if message["command"] == config["COMMANDS"]["TAKE-IMG"]:
+        if message["command"] == utils.config["COMMANDS"]["TAKE-IMG"]:
             print(f"Got valid message {message}")
-            await pub.publish(config["CHANNELS"]["CAM-COM"], data)
+            await pub.publish(utils.config["CHANNELS"]["CAM-COM"], data)
         else:
             print(f"Unknown serial command {message}")
 
 
 if __name__ == "__main__":
-    run(main)
+    utils.run(main)
