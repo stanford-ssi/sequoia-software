43f0db918d6bee8537759498e84c0de043c1c307
Langston Nashold <lgnashold@gmail.com>
2020-11-01 14:57:12 -0600

    More formatting

--- flight_computer/main.py
+++ flight_computer/main.py
@@ -5,7 +5,8 @@ from states import IdleState, LowPowerState
 import radio
 import time
 
-MIN_VOLTAGE =5 
+MIN_VOLTAGE = 5
+
 
 def initialize_state_machine():
     # create machine object of class StateMachine and add states
@@ -13,27 +14,27 @@ def initialize_state_machine():
     machine.cubesat = cubesat
 
     # Initialize Transitions
-    lowPowerTransition = LowPowerTransition('lowpower', MIN_VOLTAGE)
-    highPowerTransition = HighPowerTransition('idle', MIN_VOLTAGE)
+    low_power_transition = LowPowerTransition("lowpower", MIN_VOLTAGE)
+    high_power_transition = HighPowerTransition("idle", MIN_VOLTAGE)
 
     # Add States
-    machine.add_state(IdleState([lowPowerTransition]))
-    machine.add_state(LowPowerState([highPowerTransition]))
-    
+    machine.add_state(IdleState([low_power_transition]))
+    machine.add_state(LowPowerState([high_power_transition]))
+
     # start off the StateMachine object in idle
-    machine.go_to_state('idle')
+    machine.go_to_state("idle")
     return machine
 
 
 # Test in case we want to see if PYCUBED is working
-def LightDebuggingRoutine():
+def light_debugging_routine():
     while True:
         print("PyCubed Running")
         time.sleep(1)
         cubesat.RGB = (1, 0, 0)
         time.sleep(1)
         cubesat.RGB = (0, 0, 1)
-  
+
 
 if __name__ == "__main__":
     # Turn off light so grant can sleep
