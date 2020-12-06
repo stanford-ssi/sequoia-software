6b151af41df7f6722883e21e700700be5686b390
Yash Dalmia <yashdalmia111@gmail.com>
2020-12-05 18:19:39 -0800

    make code review changes

--- flight_computer/lib/opt3001.py
+++ flight_computer/lib/opt3001.py
@@ -28,19 +28,26 @@ DEVICE_ID = 0x7F
 REGISTER_SIZE = 2
 
 
-# EXTRACT k bits from and including pos p
-# using n ... 0 indexing
-def extract_bits(bits, pos, k):
+def extract_k_bits_from_pos(bits, k, pos):
+    """EXTRACT k bits from and including pos p, using n ... 0 indexing"""
+
     # first chop off everything exclusively after pos
     # then get k inclusive least significant bits from the 0 index
     return (bits >> pos) & ((1 << k) - 1)
+
+
+def clear_bit_at_pos(bits, pos):
+    """CLEAR bit at pos, using n...0 indexing"""
+
+    # ie perform: and 011 if you want to clear the bit at pos 2
+    return bits & (~(1 << pos))
 
 
 class Opt3001:
     """OPT3001 Sun Sensor Driver"""
 
     def __init__(self, i2c_bus, address=DEVICE_DEFAULT_I2C_ADDR, conversion_time=DEFAULT_CONVERSION_TIME):
-        # Initialize and Configure the sun sensor
+        """Initialize and Configure the Light Sensor Driver"""
 
         # Initialize i2c device from i2c passed in
         self.i2c_device = I2CDevice(i2c_bus, address)
@@ -50,32 +57,35 @@ class Opt3001:
 
         # DEVICE ID SHOULD BE 0x3001
         self.read_value(DEVICE_ID)
-        if self.buf != bytes([DEVICE_ID, 0x30, 0x01]):
+        if self.buf[1:2] != bytes([0x30, 0x01]):
             raise Exception("ERROR: Could not read correct device ID from bus provided")
 
         # MANUFACTURER ID Should be 0x5449
         self.read_value(MANUFACTURER_ID)
-        if self.buf != bytes([MANUFACTURER_ID, 0x54, 0x49]):
+        if self.buf[1:2] != bytes([0x54, 0x49]):
             raise Exception("ERROR: Could not read correct manufacturer ID from bus provided")
 
         self.configure(conversion_time)
 
-    # DeInitialize the sun sensor
     def __del__(self):
+        """DeInitialize the sun sensor: set to low power mode"""
         # put sensor in low power mode since we are done using it
-        # specifically, low power is M[1:0] set to 0b00,
-        # but we can just reset everything
-        self.buf[1] = 0x00
-        self.buf[2] = 0x00
+        # specifically, low power is M[1:0] set to 0b00
+        # M[1:0] is at pos 1 and 2 of the first configuration byte
+        # see configuration or docs for register map
+
+        self.read_value(CONFIGURATION)
+        self.buf[1] = clear_bit_at_pos(self.buf[1], 1)
+        self.buf[1] = clear_bit_at_pos(self.buf[1], 2)
+
         self.write_value(CONFIGURATION)
 
-        # TODO: is there anything else we should do? is the buffer automatically deallocated?
+        """TODO: is there anything else we should do? is the buffer automatically deallocated? """
 
-    # CONFIGURE sun sensor on startup
-    # sets conversion time equal to chosen value (800ms or 100ms)
-    # currently automatic range setting & continuous conversions
     def configure(self, conversion_time=DEFAULT_CONVERSION_TIME):
-        # automatic range setting and continuous conversions.
+        """CONFIGURE sun sensor on startup. Sets measurement time (conversion time)
+        to chosen value (800 ms or 100 ms). currently preset to automatic measurement
+        range setting & continuous conversions"""
 
         # configuration register fields:
         # RN3 RN2 RN1 RN0 CT  M1 M0  OVF
@@ -90,12 +100,12 @@ class Opt3001:
         self.buf[2] = 0x00
         self.write_value(CONFIGURATION)
 
-        # TODO: Check to make sure all other values in both buf[1] and buf[2] are desirable
+        """ TODO: Check to make sure all other values in both buf[1] and buf[2] are desirable
+            TODO: Wait for conversion ready flag to be set """
 
-        # TODO: Wait for conversion ready flag to be set
-
-    # READ value into last two bytes
     def read_value(self, register):
+        """READ value into last two bytes of buffer"""
+
         # 'with' handles locking and unlocking the i2c
         with self.i2c as i2c:
             self.buf[0] = register
@@ -106,17 +116,19 @@ class Opt3001:
             # then read but don't overwrite current register field (in_start = 1)
             i2c.stop()
 
-    # WRITE value from last two bytes
     def write_value(self, register):
+        """WRITE value from last two bytes"""
+
         # 'with' handles locking and unlocking the i2c
         with self.i2c as i2c:
             self.buf[0] = register
             i2c.write(self.buf)
             # automatically writes a stop bit
 
-    # CHECK whether sensor measurement is ready
     @property
     def conversion_ready(self):
+        """CHECK whether sensor measurement is ready"""
+
         # configuration register fields:
         # RN3 RN2 RN1 RN0 CT  M1 M0  OVF
         # CRF FH  FL  L   POL ME FC1 FC0
@@ -125,12 +137,12 @@ class Opt3001:
         # CRF (conversion ready flag, see registers below)
         # first most significant bit (last most position)
         # in the second value byte equals 1 when ready
-        return extract_bits(self.buf[2], len(self.buf[2]) - 1, 1)
+        return extract_k_bits_from_pos(self.buf[2], 1, len(self.buf[2]) - 1)
 
-    # LUX value of sun-sensor
-    # could have delay of 100ms or 800ms
     @property
     def lux(self):
+        """LUX value of sun-sensor. Could have delay up to chosen conversion time (800 or 100ms)"""
+
         # wait until the lux measurement is ready
         while not self.conversion_ready:
             time.sleep(0.1)
@@ -144,8 +156,8 @@ class Opt3001:
         # E is exponent, R is mantissa
 
         # Extract exponent and fractional result from self.buf
-        exponent = extract_bits(self.buf[1], 4, 4)  # E[3:0]
-        fractional_result = extract_bits(self.buf[1], 0, 4)  # R[12:9]
+        exponent = extract_k_bits_from_pos(self.buf[1], 4, 4)  # E[3:0]
+        fractional_result = extract_k_bits_from_pos(self.buf[1], 4, 0)  # R[12:9]
         fractional_result << 8  # pad in order to add the rest of the mantissa
         fractional_result += self.buf[2]  # R[8:1]
 
