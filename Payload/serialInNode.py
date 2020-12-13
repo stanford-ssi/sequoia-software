82722e87998d0c53211c1331e1f99cac2be78a1d
austrian-code-wizard <moritz.stephan@stanford.edu>
2020-12-13 20:04:47 +0100

    finished converting serialInNode to new wrapper

--- Payload/serialInNode.py
+++ Payload/serialInNode.py
@@ -1,6 +1,7 @@
 import json
-from utils import validate_json, get_redis_client, config, run
 import asyncio
+from redis_wrapper.app import App
+from utils import validate_json, config
 
 TEST = config["TESTING"].getboolean("TEST")
 device = None
@@ -10,22 +11,23 @@ if not TEST:
 
     device = serial.Serial("/dev/serial0", 9600)
 
+app = App("serialIn", {})
 
-async def main():
-    pub = await get_redis_client()
-    loop = asyncio.get_event_loop()
-    while True:
-        if TEST:
-            await asyncio.sleep(5)
-            data = config["TEST-MSG"]["IMG-TEST"]
-        else:
-            data = await loop.run_in_executor(None, device.read_until)
-        msg = json.loads(data)
-        if validate_json(msg):
-            await pub.publish(config["CHANNELS"]["FC-IN"], data)
-        else:
-            print(f"Invalid serial in {msg}")
+@app.interval(0)
+async def send_new_value(_, redis, cache):
+    if TEST:
+        await asyncio.sleep(5)
+        data = config["TEST-MSG"]["IMG-TEST"]
+    else:
+        loop = asyncio.get_running_loop()
+        data = await loop.run_in_executor(None, device.read_until)
+
+    msg = json.loads(data)
+    if validate_json(msg):
+        print(f"Sending to channel {config['CHANNELS']['FC-IN']}: {json.dumps(data, indent=4)}")
+        return config["CHANNELS"]["FC-IN"], data
+    return None, None
 
 
 if __name__ == "__main__":
-    run(main)
+    app.run({})
