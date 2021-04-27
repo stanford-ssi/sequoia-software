3fa39937dcb7acf8b355b1ea199eaee72416b343
Yash Dalmia <ydalmia@stanford.edu>
2021-04-27 01:26:42 -0700

    prototype for magnetorquer driver

---
+++ flight_computer/lib/magnetorquer.py
@@ -0,0 +1,12 @@
+class Magnetorquer:
+    """Induce a magnetic dipole via a magnetorquer""" 
+    
+    def __init__(self): 
+        # u = KI = NIA
+        self.K = None
+
+    def magnetic_moment(self, moment):
+        pass
+
+    def current(self, current):
+        pass
